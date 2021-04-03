7971ba9f23ae043b3d759c9accf642da5930cc48
asengsaragih <aldiwahyu.saragih@gmail.com>
2021-04-03 15:50:25 +0700

    Initial commit

---
+++ app/src/main/java/com/mobile/dental/MainActivity.java
@@ -0,0 +1,64 @@
+package com.mobile.dental;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //configure drawer menu
+        setupDrawerMenu();
+    }
+
+    private void setupDrawerMenu() {
+        //initialize component from xml
+        DrawerLayout drawer = findViewById(R.id.drawer_main);
+        NavigationView navigationView = findViewById(R.id.nav_main);
+
+        //toogle for arrow
+        mToggle = new ActionBarDrawerToggle(this, drawer, R.string.open, R.string.close);
+        drawer.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        //show burger menu
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //drawer menu clicked
+        navigationView.setNavigationItemSelectedListener(item -> {
+            selectItemDrawer(item, drawer);
+            return true;
+        });
+    }
+
+    private void selectItemDrawer(MenuItem menuItem, DrawerLayout drawerLayout) {
+        //item clicked in drawer menu
+        //kenapa pkai post delay, biar ngga ngelag ketika item di klik
+        switch (menuItem.getItemId()) {
+            case R.id.action_home:
+                drawerLayout.closeDrawers();
+                break;
+            case R.id.action_daftar:
+                drawerLayout.closeDrawers();
+                break;
+            case R.id.action_history:
+                drawerLayout.closeDrawers();
+                break;
+            case R.id.action_profile:
+                drawerLayout.closeDrawers();
+                break;
+        }
+    }
+}
