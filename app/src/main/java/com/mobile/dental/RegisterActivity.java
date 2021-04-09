e70a755f08e6da676c7251191d9b2dccdf693058
Yulio Ferdinand <yulioferdinand@gmail.com>
2021-04-09 17:08:21 +0700

    commit layout

--- app/src/main/java/com/mobile/dental/RegisterActivity.java
+++ app/src/main/java/com/mobile/dental/RegisterActivity.java
@@ -2,13 +2,68 @@ package com.mobile.dental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.mobile.dental.base.Constant;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText mDateEdittext;
+    private final Calendar mCalendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        init() ;
+    }
+
+    private void init(){
+        //inisilaisasi component
+        mDateEdittext = findViewById(R.id.ediittext_register);
+
+        //click listener
+        mDateEdittext.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ediittext_register:
+                datePickerDialog(mCalendar, mDateEdittext);
+                break;
+        }
+    }
+    private void datePickerDialog(Calendar calendar, EditText editText){
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constant.DATE_FORMAT, Locale.getDefault());
+
+        DatePickerDialog.OnDateSetListener dataSetListener = (datePicker, year, month, dayOfMonth)-> {
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+            editText.setText(simpleDateFormat.format(calendar.getTime()));
+        };
+
+        editText.setOnClickListener(view ->{
+            DatePickerDialog dialog = new DatePickerDialog(
+                    RegisterActivity.this,
+                    dataSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get((Calendar.MONTH)),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            );
+            dialog.getDatePicker().setMinDate(Calendar.getInstance().getTimeInMillis() + 86400000);
+            dialog.show();
+        });
     }
 }
---
+++ app/src/main/java/com/mobile/dental/base/Constant.java
@@ -0,0 +1,6 @@
+package com.mobile.dental.base;
+
+public class Constant {
+    public static final String DATE_FORMAT = "dd MMMM yyyy";
+
+}
