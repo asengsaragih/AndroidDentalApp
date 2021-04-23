7d5d5136b109d4ef0d0cb96c3b53fa7fe1c89b45
Yulio Ferdinand <yulioferdinand@gmail.com>
2021-04-23 16:10:58 +0700

    commit layout

--- app/src/main/java/com/mobile/dental/LoginActivity.java
+++ app/src/main/java/com/mobile/dental/LoginActivity.java
@@ -2,13 +2,39 @@ package com.mobile.dental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private View loginToRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        init();
+
+        setData();
+    }
+
+    private void init() {
+        loginToRegister = findViewById(R.id.view_login_to_register);
+    }
+
+    private void setData() {
+        loginToRegister.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.view_login_to_register:
+                startActivity(new Intent(getApplicationContext(), SignupActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/mobile/dental/ProfileActivity.java
+++ app/src/main/java/com/mobile/dental/ProfileActivity.java
@@ -1,15 +1,22 @@
 package com.mobile.dental;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mSignOutButton;
+    private CircleImageView mPictureImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +31,35 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     private void setData() {
-        mSignOutButton.setOnClickListener(this);
+        Glide.with(getApplicationContext()).load("https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half-caption/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=0hb44OrI").into(mPictureImageView);
+
     }
 
     private void init(){
-        mSignOutButton = findViewById(R.id.button_profile);
+        mPictureImageView = findViewById(R.id.imageview_profile_picture);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_sign_out:
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                break;
+            case R.id.action_edit:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.button_profile:
-                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
-                break;
-        }
+
     }
 }
