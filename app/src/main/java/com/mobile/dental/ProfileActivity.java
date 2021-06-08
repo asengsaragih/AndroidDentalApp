2c0758ab1f42e43dc277dd55c09cc4bb843154ab
Hanna <athiyyatulfarhanah14@gmail.com>
2021-06-08 21:58:52 +0700

    API get profile, update profile, dan sign out.

--- app/src/main/java/com/mobile/dental/ProfileActivity.java
+++ app/src/main/java/com/mobile/dental/ProfileActivity.java
@@ -9,15 +9,30 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.mobile.dental.base.BaseActivity;
+import com.mobile.dental.base.Constant;
+import com.mobile.dental.model.Auth;
+import com.mobile.dental.model.Profile;
+
+import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ProfileActivity extends BaseActivity implements View.OnClickListener {
 
-    private CircleImageView mPictureImageView;
+    private Profile PROFILE_DATA = null;
+
+    private TextView mUIDTextView;
+    private TextView mFullnameTextView;
+    private TextView mUsernameTextView;
+    private TextView mEmailTextView;
+    private TextView mKontakTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +46,52 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
         setData();
     }
 
-    private void setData() {
-        Glide.with(getApplicationContext()).load("https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half-caption/public/field_blog_entry_images/2018-09/shutterstock_648907024.jpg?itok=0hb44OrI").into(mPictureImageView);
+    private void init(){
+        //init component dari layout
 
+        mUIDTextView = findViewById(R.id.textview_profile_uid);
+        mFullnameTextView = findViewById(R.id.textview_profile_fullname);
+        mUsernameTextView = findViewById(R.id.textview_profile_username);
+        mEmailTextView = findViewById(R.id.textview_profile_email);
+        mKontakTextView = findViewById(R.id.textview_profile_kontak);
     }
 
-    private void init(){
-        mPictureImageView = findViewById(R.id.imageview_profile_picture);
+    private void setData() {
+        //get profile dari api
+        getProfile();
+    }
 
+    private void getProfile() {
+        //get profile dari api
+        showLoading(true);
+
+        Call<List<Profile>> getProfileCall = mApiService.getUser(mSession.getAuthSession().getId());
+        getProfileCall.enqueue(new Callback<List<Profile>>() {
+            @Override
+            public void onResponse(Call<List<Profile>> call, Response<List<Profile>> response) {
+                if (response.code() != 200 || response.body() == null || response.body().size() == 0){
+                    showLoading(false);
+                    toast("Gagal memuat data");
+                } else {
+                    showLoading(false);
+                    Profile profile = response.body().get(0);
+
+                    //masukkan kedalam tmp profile untuk edit data
+                    PROFILE_DATA = profile;
+
+                    mUIDTextView.setText(profile.getIdUser());
+                    mFullnameTextView.setText(profile.getFullname());
+                    mUsernameTextView.setText(profile.getUsername());
+                    mEmailTextView.setText(profile.getEmail());
+                    mKontakTextView.setText(profile.getKontak());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Profile>> call, Throwable t) {
+                showLoading(false);
+            }
+        });
     }
 
     @Override
@@ -51,13 +104,36 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.action_sign_out:
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                logout();
                 break;
             case R.id.action_edit:
-                startActivity(new Intent(getApplicationContext(), UpdateProfileActivity.class));
+                updateProfile();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateProfile() {
+        if (PROFILE_DATA == null) {
+            getProfile();
+        } else {
+            Intent intent = new Intent(getApplicationContext(), UpdateProfileActivity.class);
+            intent.putExtra(Constant.INTENT_UPDATE_PROFILE, PROFILE_DATA);
+            startActivity(intent);
+        }
+    }
+
+    private void logout() {
+        //clear session
+        Auth auth = new Auth(null, null,
+                null, null, null,
+                null);
+        mSession.setAuthSession(auth);
+
+        //arahkan ke login activity dan ditambahkan flag agar tidak bisa balik ke profile activity
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     @Override
--- app/src/main/java/com/mobile/dental/UpdateProfileActivity.java
+++ app/src/main/java/com/mobile/dental/UpdateProfileActivity.java
@@ -2,15 +2,124 @@ package com.mobile.dental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.mobile.dental.base.BaseActivity;
+import com.mobile.dental.base.Constant;
+import com.mobile.dental.model.Profile;
+import com.mobile.dental.model.UpdateProfileResult;
 
-public class UpdateProfileActivity extends BaseActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class UpdateProfileActivity extends BaseActivity implements View.OnClickListener {
+
+    private EditText mFullnameEdittext;
+    private EditText mUsernameEdittext;
+    private EditText mPasswordEdittext;
+    private EditText mEmailEdittext;
+    private EditText mContactEdittext;
+
+    private Button mUpdateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+
+        init();
+        setData();
+    }
+
+    private void init() {
+        mFullnameEdittext = findViewById(R.id.edittext_update_profile_fullname);
+        mUsernameEdittext = findViewById(R.id.edittext_update_profile_username);
+        mPasswordEdittext = findViewById(R.id.edittext_update_profile_password);
+        mEmailEdittext = findViewById(R.id.edittext_update_profile_email);
+        mContactEdittext = findViewById(R.id.edittext_update_profile_kontak);
+
+        mUpdateButton = findViewById(R.id.button_update_profile_save);
+    }
+
+    private void setData() {
+        mUpdateButton.setOnClickListener(this);
+
+        //set data untuk edittext
+        Profile profile = getProfileData();
+
+        mFullnameEdittext.setText(profile.getFullname());
+        mUsernameEdittext.setText(profile.getUsername());
+        mPasswordEdittext.setText(profile.getPassword());
+        mEmailEdittext.setText(profile.getEmail());
+        mContactEdittext.setText(profile.getKontak());
+
+    }
+
+    private Profile getProfileData() {
+        //mengambil data intent dari profile activity
+        return (Profile) getIntent().getSerializableExtra(Constant.INTENT_UPDATE_PROFILE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_update_profile_save:
+                updateProfile();
+                break;
+        }
+    }
+
+    private void updateProfile() {
+        //fungsi untuk update profile
+        if (isEdittextEmpty(mFullnameEdittext) ||
+                isEdittextEmpty(mUsernameEdittext) ||
+                isEdittextEmpty(mPasswordEdittext) ||
+                isEdittextEmpty(mEmailEdittext) ||
+                isEdittextEmpty(mContactEdittext)) {
+            toast("Field tidak boleh kosong");
+            return;
+        }
+
+        String uid = getProfileData().getIdUser();
+        String fullname = mFullnameEdittext.getText().toString();
+        String username = mUsernameEdittext.getText().toString();
+        String password = mPasswordEdittext.getText().toString();
+        String email = mEmailEdittext.getText().toString();
+        String contact = mContactEdittext.getText().toString();
+
+        showLoading(true);
+
+        Call<UpdateProfileResult> updateCall = mApiService.updateProfile(uid, fullname, username, password, email, contact);
+        updateCall.enqueue(new Callback<UpdateProfileResult>() {
+            @Override
+            public void onResponse(Call<UpdateProfileResult> call, Response<UpdateProfileResult> response) {
+                showLoading(false);
+
+                if (response.code() == 200 && response.body() != null){
+                    toast(response.body().getMessage());
+
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    finish();
+                } else {
+                    toast("error update data");
+
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    finish();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UpdateProfileResult> call, Throwable t) {
+                showLoading(false);
+            }
+        });
     }
 }
